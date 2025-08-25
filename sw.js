0755a21177e25ce945a988b34b06941176a47668
sommeralisson <alissonsommer1@gmail.com>
2025-08-25 19:41:36 -0300

    configuração pwa

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/public/images/pizzaBacon.png',
+  '/public/images/pizzaBanner.jpg',
+  '/public/images/pizzaBrocolis.png',
+  '/public/images/pizzaCalabresa.png',
+  '/public/images/pizzaCarne.png',
+  '/public/images/pizzaChocolate.png',
+  '/public/images/pizzaFrango.png',
+  '/public/images/pizzaLogo.webp',
+  '/public/images/pizzaPepperoni.png',
+  '/public/images/pizzaQueijo.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna a resposta do cache se encontrada
+        if (response) {
+          return response;
+        }
+        // Caso contrário, busca na rede
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
